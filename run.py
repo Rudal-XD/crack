f1502090e8d608dc6f075b56043ff4a1a74083c6
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-08-06 05:38:53 +0700

    Update run.py

--- run.py
+++ run.py
@@ -198,23 +198,46 @@ def login():
 
 		
 
-def menu():
+class menu:
 
-	try:ls = requests.get('https://api.ipify.org')
-
-	except:ls = {'origin':'-'}
-
-	try:
-
-		tglx = my_birthday.split('/')[1]
-
-		blnx = dic2[str(my_birthday.split('/')[0])]
-
-		thnx = my_birthday.split('/')[2]
-
-		birth = tglx+' '+blnx+' '+thnx
-
-	except:birth = '-'
+	def __init__(self):
+		self.uid = []
+	def main(self):
+		os.system('clear')
+		try:
+			toke = open('token.x','r').read()
+		except IOError:
+			print(' [%s+%s] Kamu belum login'%(M,N));login().__login__()
+		try:
+			r = requests.get('https://graph.facebook.com/me?access_token=%s'%(toke)).json()['name']
+		except KeyError:
+			print(' [%s!%s] Login gagal ...'%(M,N));os.system('rm -rf token.x');time.sleep(2);login().__login__()
+		except requests.exceptions.ConnectionError:
+			exit(' [%s!%s] cek koneksi'%(M,N))
+		try:
+			akss = open('license.txt','r').read()
+		except IOError:
+			akss = '-'
+		banner()
+		IP = requests.get('https://api.ipify.org').text
+		jalan(' %s[ %sselamat Datang %s%s ]'%(N,H,r,N))
+		print(' %s[%s•%s] Alamat IP kamu saat ini : %s'%(H,O,H,IP))
+		print(' %s[%s•%s] Kamu masuk pada         : %s'%(N,O,N,waktu))
+		print(' %s'%(N))
+		print(' %s[%s0%s] crack dari daftar teman'%(N,O,N))
+		print(' %s[%s1%s] crack dari akun publik'%(N,O,N))
+		print(' %s[%s2%s] crack dari akun massal'%(N,O,N))
+		print(' %s[%s3%s] crack dari postingan'%(N,O,N))
+		print(' %s[%s4%s] crack dari likes post'%(N,O,N))
+		print(' %s[%s5%s] crack dari followers'%(N,O,N))
+		print(' %s[%s6%s] cek opsi akun chekpoint'%(N,O,N))
+		print(' %s[%s7%s] cek hasil crack ok,cp'%(N,O,N))
+		print(' %s[%s8%s] seting User-Agent'%(N,O,N))
+		print(' %s[%s9%s] crack email'%(N,O,N))
+		print(' %s[%sG%s] Get data² facebook'%(N,O,N))
+		print(' %s[%sK%s] Lapor bug script'%(N,O,N))
+		print(' %s[%sA%s] Keluar, hapus token'%(N,O,N))
+		self.pilih()
 
 	def pilih(self):
 
