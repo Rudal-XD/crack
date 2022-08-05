9ff5f8821835c91c04165cf75565642490a932f3
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-08-05 21:36:56 +0700

    Create run.py

--- run.py
+++ run.py
@@ -132,7 +132,6 @@ B = random.choice([U,I,K,b,M])
 
 balmond = O+"["+J+"•"+O+"]"
 
-
 dic = {'1':'Januari','2':'Februari','3':'Maret','4':'April','5':'Mei','6':'Juni','7':'Juli','8':'Agustus','9':'September','10':'Oktober','11':'November','12':'Desember'}
 
 dic2 = {'01':'Januari','02':'Februari','03':'Maret','04':'April','05':'Mei','06':'Juni','07':'Juli','08':'Agustus','09':'September','10':'Oktober','11':'November','12':'Desember'}
@@ -246,12 +245,6 @@ def license():#line:42
     else :#line:64
 
       exit()
-
-
-
-balmond = O+"["+J+"•"+O+"]"
-
-
 
 def login():
 
