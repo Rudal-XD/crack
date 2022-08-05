a9614b6a57516a724332b63841c63972392c24bd
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-08-05 21:35:58 +0700

    Update run.py

--- run.py
+++ run.py
@@ -130,6 +130,7 @@ war = "[•]"
 
 B = random.choice([U,I,K,b,M])
 
+balmond = O+"["+J+"•"+O+"]"
 
 
 dic = {'1':'Januari','2':'Februari','3':'Maret','4':'April','5':'Mei','6':'Juni','7':'Juli','8':'Agustus','9':'September','10':'Oktober','11':'November','12':'Desember'}
